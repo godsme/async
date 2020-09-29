c035d0826016894ed437f6e67b0993df9f6ddc0c
Darwin Yuan <darwin_yuan@hotmail.com>
2020-09-29 20:53:38 +0800

    refactored future callback

--- include/async/detail/future_callback_object.h
+++ include/async/detail/future_callback_object.h
@@ -21,48 +21,21 @@ namespace detail {
       using super::super;
 
       auto on_future_ready() noexcept -> void override {
-         super::set_value(super::f_(super::subject_->get_value()));
-         super::do_commit();
-      }
-   };
-
-   /////////////////////////////////////////////////////////////////////////////////////////////////
-   template<typename F, typename A>
-   struct future_callback_object<void, F, A, std::enable_if_t<std::is_invocable_r_v<void, std::decay_t<F>, A const &>>>
-      : future_callback_base<void, F, A> {
-      using super = future_callback_base<void, F, A>;
-      using super::super;
-
-      auto on_future_ready() noexcept -> void override {
-         super::f_(super::subject_->get_value());
-         super::set_value();
-         super::do_commit();
-      }
-   };
-
-   /////////////////////////////////////////////////////////////////////////////////////////////////
-   template<typename R, typename F>
-   struct future_callback_object<R, F, void, std::enable_if_t<std::is_invocable_r_v<R, std::decay_t<F>>>>
-      : future_callback_base<R, F, void> {
-      using super = future_callback_base<R, F, void>;
-      using super::super;
-
-      auto on_future_ready() noexcept -> void override {
-         future_object<R>::set_value(super::f_());
-         super::do_commit();
-      }
-   };
-
-   /////////////////////////////////////////////////////////////////////////////////////////////////
-   template<typename F>
-   struct future_callback_object<void, F, void, std::enable_if_t<std::is_invocable_r_v<void, std::decay_t<F>>>>
-      : future_callback_base<void, F, void> {
-      using super = future_callback_base<void, F, void>;
-      using super::super;
-
-      auto on_future_ready() noexcept -> void override {
-         super::f_();
-         future_object<void>::set_value();
+         if constexpr (std::is_void_v<R>) {
+            if constexpr (std::is_void_v<A>) {
+               super::f_();
+               future_object<void>::set_value();
+            } else {
+               super::f_(super::subject_->get_value());
+               super::set_value();
+            }
+         } else {
+            if constexpr (std::is_void_v<A>) {
+               future_object<R>::set_value(super::f_());
+            } else {
+               super::set_value(super::f_(super::subject_->get_value()));
+            }
+         }
          super::do_commit();
       }
    };
